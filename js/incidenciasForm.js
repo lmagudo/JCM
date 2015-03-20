5076608dfc6b451a6da5095708dfd295a31828d5
trucheromayor <jon.garrido@gmail.com>
2015-03-20 18:12:14 +0100

    zoom to projected

--- js/incidenciasForm.js
+++ js/incidenciasForm.js
@@ -212,6 +212,26 @@
             function zoomtoResult(result) {
                 console.log(result);
                 TwoCartoMap.setExtent(result.features[0].geometry.getExtent(), true);
+
+
+
+
+                ///geometry service...
+                require(["esri/SpatialReference"], function (SpatialReference) {
+
+                    var gsvc = new GeometryService("http://qvialweb.es:6080/arcgis/rest/services/Utilities/Geometry/GeometryServer");
+                    var outSR = new SpatialReference(102100);
+                    gsvc.project([result.features[0].geometry], outSR, function (projectedGeometry) {
+                        TwoCartoMap.setExtent(projectedGeometry, true);
+                    });
+                
+                
+                });
+
+
+                
+                
+
             }
 
         }
